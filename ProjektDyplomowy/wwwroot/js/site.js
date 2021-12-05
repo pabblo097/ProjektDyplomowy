039719c3cf96f33ced039854915af2f4925c82e6
Pawel Wiewiora <kaczka097@gmail.com>
2021-12-05 21:36:20 +0100

    dodano funkcjonalnosc zmiany motywu strony

--- ProjektDyplomowy/wwwroot/js/site.js
+++ ProjektDyplomowy/wwwroot/js/site.js
@@ -2,3 +2,47 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+//THEME TOGGLER=============================================
+const darkTheme = document.querySelector("#darkTheme");
+const lightTheme = document.querySelector("#lightTheme");
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+        document.getElementById('slider').checked = true;
+    }
+})();
+
+function setTheme(theme) {
+    if (theme === "theme-dark") {
+        darkTheme.rel = 'stylesheet';
+        lightTheme.rel = 'alternate stylesheet';
+        localStorage.setItem("theme", "theme-dark");
+    } else {
+        darkTheme.rel = 'alternate stylesheet';
+        lightTheme.rel = 'stylesheet';
+        localStorage.setItem("theme", "theme-light");
+    }
+}
+//===============================================================
