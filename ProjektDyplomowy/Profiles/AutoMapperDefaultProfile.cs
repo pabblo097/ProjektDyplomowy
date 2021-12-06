3a7dbe98da121b87e3c05f4c8608f0294f757475
Pawel Wiewiora <kaczka097@gmail.com>
2021-12-06 22:53:03 +0100

    dodano domyslny profil AutoMappera

---
+++ ProjektDyplomowy/Profiles/AutoMapperDefaultProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjektDyplomowy.Entities;
+
+namespace ProjektDyplomowy.Profiles
+{
+    public class AutoMapperDefaultProfile : Profile
+    {
+        public AutoMapperDefaultProfile()
+        {
+            CreateMap<Category, SelectListItem>()
+                .ForMember(des => des.Text, opt => opt.MapFrom(src => src.Name))
+                .ForMember(des => des.Value, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
