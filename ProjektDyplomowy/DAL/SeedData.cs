5ca7a6e26a13fee25905a9be30b01e517ae27572
Pawel Wiewiora <kaczka097@gmail.com>
2021-12-07 01:45:05 +0100

    dodano seedowanie admina i rol

---
+++ ProjektDyplomowy/DAL/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjektDyplomowy.Entities;
+
+namespace ProjektDyplomowy.DAL
+{
+    public class SeedData
+    {
+        public static async Task SeedRolesAsync(RoleManager<Role> roleManager)
+        {
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new Role()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Admin"
+                });
+            }
+            if (!await roleManager.RoleExistsAsync("Moderator"))
+            {
+                await roleManager.CreateAsync(new Role()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Moderator"
+                });
+            }
+            if (!await roleManager.RoleExistsAsync("User"))
+            {
+                await roleManager.CreateAsync(new Role()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "User"
+                });
+            }
+        }
+
+        public static async Task SeedAdminAsync(UserManager<User> userManager)
+        {
+            //Seed Default User(Admin)
+            var defaultAdmin1 = new User
+            {
+                Id = Guid.NewGuid(),
+                UserName = "admin2137",
+                Email = "admin@gmail.com",
+                PhoneNumber = "123456789",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+            };
+            if (userManager.Users.All(u => u.Id != defaultAdmin1.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultAdmin1.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultAdmin1, "Haslo1");
+                    await userManager.AddToRoleAsync(defaultAdmin1, "Admin");
+                }
+            }
+        }
+    }
+}
--- ProjektDyplomowy/Program.cs
+++ ProjektDyplomowy/Program.cs
@@ -7,26 +7,26 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//EF Core Config
+// EF Core Config
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
-//=====================================================
 
 
-//ASP.NET Core Identity Config
+// ASP.NET Core Identity Config
 builder.Services.AddDefaultIdentity<User>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
 })
 .AddRoles<Role>()
 .AddEntityFrameworkStores<AppDbContext>()
-.AddDefaultTokenProviders();
-//=====================================================
+.AddDefaultTokenProviders()
+.AddErrorDescriber<PolishIdentityErrorDescriber>();
 
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -35,11 +35,31 @@ builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 builder.Services.AddScoped<IPostsRepository, PostsRepository>();
 
 
-//========================================
+//====================================================
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Seed basic data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var roleManager = services.GetRequiredService<RoleManager<Role>>();
+        await SeedData.SeedRolesAsync(roleManager);
+        await SeedData.SeedAdminAsync(userManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
+
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
