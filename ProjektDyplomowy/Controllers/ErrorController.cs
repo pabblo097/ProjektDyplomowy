0602f925a6331bbf25914e4bfd6ab0ca5813dc07
Pawel Wiewiora <kaczka097@gmail.com>
2021-12-21 17:34:20 +0100

    dodano strone z błędem 404

---
+++ ProjektDyplomowy/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjektDyplomowy.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("[controller]/404")]
+        public IActionResult Error404()
+        {
+            return View("404");
+        }
+    }
+}
