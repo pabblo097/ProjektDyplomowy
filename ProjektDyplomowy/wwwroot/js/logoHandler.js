393c4e646098eb8677b4262b6e58f742aecddb6e
Pawel Wiewiora <kaczka097@gmail.com>
2022-01-06 19:52:15 +0100

    poprawki w wyglądzie

---
+++ ProjektDyplomowy/wwwroot/js/logoHandler.js
@@ -0,0 +1,11 @@
+﻿const miniLogo = document.querySelector(".navbar-brand");
+
+document.addEventListener("scroll", () => {
+    if (window.scrollY >= 178) {
+        miniLogo.classList.remove("d-lg-none");
+    }
+    else {
+        miniLogo.classList.add("d-lg-none");
+
+    }
+})
